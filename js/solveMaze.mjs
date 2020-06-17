daed2d9816b0b3cd971ab7a8c90ab86ba954dcaf
Jan Koupil <jan.koupil@gmail.com>
2020-06-17 08:10:47 +0200

    join dynamic structures import

--- js/solveMaze.mjs
+++ js/solveMaze.mjs
@@ -2,9 +2,7 @@ import {Vector} from "./vector.mjs";
 import {MazeView} from "./mazeView.mjs";
 import {Maze} from "./maze.mjs";
 
-import {Queue} from "./dynamicStructures.mjs";
-import {Stack} from "./dynamicStructures.mjs";
-import {SortList} from "./dynamicStructures.mjs";
+import {Queue, Stack, SortList} from "./dynamicStructures.mjs";
 
 // import {map, start, exit} from "./data/simpleMaze.mjs";
 import {map, start, exit} from "./data/largeMaze.mjs";
